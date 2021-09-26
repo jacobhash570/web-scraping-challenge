5219d2033286f9bb9d63ac9ee09333f05612b54e
Jacob <jacob.hash570@gmail.com>
2021-09-26 13:18:08 -0400

    Mars facts.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,5 +1,6 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 def init_browser():
     executable_path = {'executable_path': 'chromedriver'}
@@ -27,3 +28,11 @@ def scrape():
     image_url= soup.find('img', class_='headerimage fade-in')['src'] 
 
     featured_image_url = nasa_url + image_url
+
+    # Mars Facts 
+    url = 'https://galaxyfacts-mars.com/'
+    facts_table = pd.read_html(url)
+    facts_df = facts_table[1]
+    facts_df.columns = ["Category", "Measurement"]
+    facts_df = facts_df.set_index("Category")
+    
